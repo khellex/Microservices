e83e50cf2b758ed5a0a26639a9690de317bd3686
Karthik Pai <kpai1996@gmail.com>
2024-11-08 16:18:11 +0530

    Added Cart checkout UI

--- Mango/Mango.Web/Controllers/CartController.cs
+++ Mango/Mango.Web/Controllers/CartController.cs
@@ -20,6 +20,12 @@ namespace Mango.Web.Controllers
         {
             return View(await LoadCartDtoBasedOnLoggedInUser());
         }
+        [Authorize]
+        public async Task<IActionResult> Checkout()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
         private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
         {
             var loggedInUserId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
